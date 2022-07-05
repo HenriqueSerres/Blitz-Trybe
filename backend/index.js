e9b40004371738bb0cda4f08b633012d2a581d6d
HenriqueSerres <henriqueserres@gmail.com>
2022-07-05 11:37:29 -0300

    add cors backend

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const router = require('./routes');
 const middlewares = require('./middlewares/errorMiddleware');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(router);
 app.use(middlewares.errorMiddleware);
